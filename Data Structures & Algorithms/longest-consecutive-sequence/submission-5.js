36c76fc0a19357d690fb705f14978cfa6f0b9685
Maksym <maksimbaluta02@gmail.com>
2026-07-06 10:50:43 +0200

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+
+        // Brute force solution
+        if (!nums.length) {
+            return 0;
+        }
+
+        if(nums.length === 1) {
+            return 1
+        }
+
+        const sortedArr = nums.sort((a, b) => a - b);
+        const res = []
+        let count = 1;
+
+        for (let i = 0; i < sortedArr.length - 1; i++) {
+            const curr = sortedArr[i];
+            const next = sortedArr[i + 1];
+
+            if (curr - next === -1) {
+                count += 1;
+            }else if(curr - next < -1) {
+                count = 1
+            }
+            res.push(count)
+        }
+        
+        return Math.max(...res);
+    }
+}

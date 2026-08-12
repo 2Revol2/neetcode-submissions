7d637a428315105795bcaf67d30cf9ac42d3118a
Maksym <maksimbaluta02@gmail.com>
2026-08-12 12:15:05 +0200

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+
+        for (let i = 0; i < tokens.length; i++) {
+            const num = Number(tokens[i]);
+            if (!Number.isNaN(num)) {
+                stack.push(num);
+            } else {
+                let val1 = stack.pop();
+                let val2 = stack.pop();
+                if (tokens[i] === "+") {
+                    stack.push(val1 + val2);
+                } else if (tokens[i] === "*") {
+                    stack.push(val1 * val2);
+                } else if (tokens[i] === "-") {
+                    stack.push(val2 - val1);
+                } else if (tokens[i] === "/") {
+                    stack.push(Math.trunc(val2 / val1));
+                }
+            }
+        }
+
+        return stack[0];
+    }
+}

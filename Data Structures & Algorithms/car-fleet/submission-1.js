a2fdfb204616dfe17ff33d4ef47e645b02eb59c0
Maksym <maksimbaluta02@gmail.com>
2026-08-15 10:47:02 +0200

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let res = []
+
+        
+        for(let i = 0; i < position.length; i++) {
+
+            let hours = (target - position[i])/speed[i]
+
+            if(!res.includes(hours)) {
+                res.push(hours)
+            }
+        }
+        return res.length
+    }
+}

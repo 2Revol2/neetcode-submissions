952eacde466f003ae480bae7545e5b63a5e867f4
Maksym <maksimbaluta02@gmail.com>
2026-09-06 10:15:02 +0200

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b)
+        let res = [];
+
+        for(let i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] === nums[i - 1]) {
+                continue
+            } 
+
+            let l = i + 1;
+            let r = nums.length - 1;
+
+            while(l < r) {
+                const sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0) {
+                    l++;
+                }else if(sum > 0) {
+                    r--;
+                }else {
+                    res.push([nums[i], nums[l], nums[r]])
+                    l++;
+                    while(nums[l] === nums[l - 1] && l < r) {
+                        l++;
+                    }
+                }
+            }
+        }
+
+        return res
+    }
+}
